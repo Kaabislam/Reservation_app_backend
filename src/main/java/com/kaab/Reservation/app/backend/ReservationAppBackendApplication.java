cdf0a40cd0e7df9b2818b13fbcc4adbe8a2919f5
Kaabislam <islamkaab13@gmail.com>
2024-10-10 19:54:08 +0600

    add test to work with jenkins

--- src/main/java/com/kaab/Reservation/app/backend/ReservationAppBackendApplication.java
+++ src/main/java/com/kaab/Reservation/app/backend/ReservationAppBackendApplication.java
@@ -1,12 +1,22 @@
 package com.kaab.Reservation.app.backend;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class ReservationAppBackendApplication {
-
+	public static Logger logger = (Logger) LoggerFactory.getLogger(ReservationAppBackendApplication.class);
+	@PostConstruct
+	public void init(){
+		logger.info("Application started...");
+	}
 	public static void main(String[] args) {
+		logger.info("Application Executed...");
+
 		SpringApplication.run(ReservationAppBackendApplication.class, args);
 	}
 
--- src/test/java/com/kaab/Reservation/app/backend/ReservationAppBackendApplicationTests.java
+++ src/test/java/com/kaab/Reservation/app/backend/ReservationAppBackendApplicationTests.java
@@ -1,13 +1,21 @@
 package com.kaab.Reservation.app.backend;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ReservationAppBackendApplicationTests {
-
+	public static Logger logger = (Logger) LoggerFactory.getLogger(ReservationAppBackendApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("Test Case executing...");
+		assertEquals(true,true);
 	}
 
 }
